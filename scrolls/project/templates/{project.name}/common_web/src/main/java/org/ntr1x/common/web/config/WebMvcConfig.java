ec641f854afe83af9faff43b8897b5a9e9cdb008
Igor Pavlenko <i.pavlenko@bars-open.ru>
2024-03-31 18:22:16 +0500

    #1 Set up OpenID integration

--- scrolls/project/templates/{project.name}/common_web/src/main/java/org/ntr1x/common/web/config/WebMvcConfig.java
+++ scrolls/project/templates/{project.name}/common_web/src/main/java/org/ntr1x/common/web/config/WebMvcConfig.java
@@ -1,9 +1,11 @@
 package org.ntr1x.common.web.config;
 
 import org.ntr1x.common.api.component.AppConverter;
+import org.ntr1x.common.web.component.AppArgumentResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,13 +14,27 @@ import java.util.List;
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
-    @Autowired
+    @Autowired(required = false)
     private List<AppConverter<?, ?>> converters;
+
+    @Autowired(required = false)
+    private List<AppArgumentResolver> argumentResolvers;
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        for (AppConverter converter : converters) {
-            registry.addConverter(converter);
+        if (converters != null) {
+            for (AppConverter converter : converters) {
+                registry.addConverter(converter);
+            }
+        }
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        if (argumentResolvers != null) {
+            for (AppArgumentResolver resolver : argumentResolvers) {
+                resolvers.add(resolver);
+            }
         }
     }
 }
