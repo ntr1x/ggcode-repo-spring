dbacb29a15bfedd7f134006df148d2801eb5e762
Igor Pavlenko <i.pavlenko@bars-open.ru>
2024-04-22 16:48:19 +0500

    #3 Provide ability to override model request model fields

--- scrolls/project/templates/{project.name}/common_events/src/main/java/org/ntr1x/common/events/config/CloudEventsKafkaConfig.java
+++ scrolls/project/templates/{project.name}/common_events/src/main/java/org/ntr1x/common/events/config/CloudEventsKafkaConfig.java
@@ -6,18 +6,18 @@ import io.cloudevents.kafka.CloudEventSerializer;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.ntr1x.common.events.CommonEventsConstants;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.ntr1x.common.events.CloudEventsConstants;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.*;
 
-@AutoConfiguration
+@Configuration
 @RequiredArgsConstructor
-public class CommonEventsKafkaConfig {
+public class CloudEventsKafkaConfig {
     private final KafkaProperties kafkaProperties;
 
-    @Bean(CommonEventsConstants.CONSUMER_FACTORY_CLOUD_EVENT)
+    @Bean(CloudEventsConstants.CONSUMER_FACTORY_CLOUD_EVENT)
     public ConsumerFactory<String, CloudEvent> cloudEventConsumerFactory() {
         return new DefaultKafkaConsumerFactory<>(
                 kafkaProperties.buildConsumerProperties(),
@@ -26,7 +26,7 @@ public class CommonEventsKafkaConfig {
         );
     }
 
-    @Bean(CommonEventsConstants.PRODUCER_FACTORY_CLOUD_EVENT)
+    @Bean(CloudEventsConstants.PRODUCER_FACTORY_CLOUD_EVENT)
     public ProducerFactory<String, CloudEvent> cloudEventProducerFactory() {
         return new DefaultKafkaProducerFactory<>(
                 kafkaProperties.buildProducerProperties(),
@@ -35,7 +35,7 @@ public class CommonEventsKafkaConfig {
         );
     }
 
-    @Bean(CommonEventsConstants.KAFKA_TEMPLATE_CLOUD_EVENT)
+    @Bean(CloudEventsConstants.KAFKA_TEMPLATE_CLOUD_EVENT)
     public KafkaTemplate<String, CloudEvent> cloudEventKafkaTemplate() {
         return new KafkaTemplate<>(cloudEventProducerFactory());
     }
--- scrolls/project/templates/{project.name}/common_events/src/main/java/org/ntr1x/common/events/contributor/CommonEventsContainerContributor.java
+++
@@ -1,9 +0,0 @@
-package org.ntr1x.common.events.contributor;
-
-import org.springframework.kafka.listener.MessageListenerContainer;
-
-import java.util.Map;
-
-public interface CommonEventsContainerContributor {
-    void addContainers(Map<String, MessageListenerContainer> containers);
-}
