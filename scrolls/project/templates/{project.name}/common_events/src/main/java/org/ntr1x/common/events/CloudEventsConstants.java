3a3c2940f237dddcbfd25dfea8b28c2563ebea9a
Igor Pavlenko <i.pavlenko@bars-open.ru>
2024-04-25 10:50:59 +0500

    #3 Provide ability to override model request model fields

--- scrolls/project/templates/{project.name}/common_events/src/main/java/org/ntr1x/common/events/CloudEventsConstants.java
+++ scrolls/project/templates/{project.name}/common_events/src/main/java/org/ntr1x/common/events/CloudEventsConstants.java
@@ -4,4 +4,5 @@ public class CloudEventsConstants {
     public static final String PRODUCER_FACTORY_CLOUD_EVENT = "cloudEventProducerFactory";
     public static final String CONSUMER_FACTORY_CLOUD_EVENT = "cloudEventConsumerFactory";
     public static final String KAFKA_TEMPLATE_CLOUD_EVENT = "cloudEventKafkaTemplate";
+    public static final String CONTAINER_FACTORY_CLOUD_EVENT = "cloudEventContainerFactory";
 }
